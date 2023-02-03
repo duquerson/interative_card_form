d483a4b50b6ace5d68d04c7dccdb72b8c9f5d2be
Duquerson <sotodjgato@hotmail.com>
2023-02-02 22:17:53 -0300

    changes test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './style.scss';
+import './desktop.scss';
 import SuperExpressive from 'super-expressive';
 
 //nombre de la tarjeta 
@@ -10,15 +11,14 @@ let nameError = document.querySelector('.errorName');
 //regex y manejador de eventos
 
 let number = SuperExpressive().allowMultipleMatches.digit.toRegex();
-let nn = /\d/g;
 
 
 nameInput.addEventListener('input', ()=>{
     (nameInput.value=== '')?
-        nameCard.innerText = 'JANE APPLESEED'
-        : nameCard.innerText = nameInput.value;
+        nameCard.textContent = 'JANE APPLESEED'
+        : nameCard.textContent = nameInput.value;
     if(nameInput.value.match(number) !== null){
-        nameError.innerText = 'Wrong format, characters only';
+        nameError.textContent = 'Wrong format, characters only';
         nameInput.classList.add('cardholder');
         nameError.classList.remove('error');     
     }else{
@@ -36,21 +36,48 @@ let numberError = document.querySelector('.errorNumber');
 //regex y manejador de eventos
 
 let caracteres = /[A-z]/g;
-let add = /([0-9]{4})/g;
+let add = /([0-9]{4})/g; //grupo de 4 numeros 
 let space = SuperExpressive().whitespaceChar.allowMultipleMatches.toRegex();
+
 numberInput.addEventListener('input',(event)=>{
     let input = event.target.value;//seleciono texto escrito
     
-    numberCard.innerText = numberInput.value;
-
     if(numberInput.value.match(caracteres) !== null){
-        numberError.innerText = 'Wrong format, numbers only';
+        numberError.textContent = 'Wrong format, numbers only';
         numberInput.classList.add('cardholder');
         numberError.classList.remove('error');
     }else{
-        numberInput.value = input.replace(space, '').replace(add, '$1 ').trim();
+        numberInput.value = input.replace(space, '').replace(add, '$1 ').trim();//$1 es la subcadana representada en el regex 
         numberInput.classList.remove('cardholder');
         numberError.classList.add('error');
     }
-    if (numberInput.value === '') numberCard.innerText = '0000 0000 0000 0000';
+    (numberInput.value === '')? numberCard.textContent = '0000 0000 0000 0000': numberCard.textContent = numberInput.value;
+});
+
+//mes tarjeta
+
+let cardmonth = document.querySelector('.card__month');
+let inputmonth = document.querySelector('.form__label--input');
+let montherror = document.querySelector('.errorDate');
+
+//regex y manejador de eventos
+
+inputmonth.addEventListener('input', ()=>{
+    (inputmonth.value === '')?
+        cardmonth.textContent = '00':
+        cardmonth.textContent = inputmonth.value;
+        
+    if(inputmonth.value.match(number) !== null){
+        montherror.textContent = 'Wrong format';
+        inputmonth.classList.add('cardholder');
+        inputmonth.classList.remove('error');
+    }else{
+        inputmonth.classList.remove('cardholder');
+        inputmonth.classList.add('error');
+    }
+
+
+
+
+
 });
