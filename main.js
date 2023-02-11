2dccc6f55e293b7c9737abad950dbe4b217436ee
Duquerson <sotodjgato@hotmail.com>
2023-02-10 21:55:17 -0300

    changes of desing validation

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 import './style.scss';
 import './desktop.scss';
+import './nodos.js';
 import SuperExpressive from 'super-expressive';
 
-//nombre de la tarjeta 
-
-let nameCard = document.querySelector('.card__details--name');
-let nameInput = document.querySelector('#cardholder');
-let nameError = document.querySelector('.errorName');
 
 //regex y manejador de eventos
 
@@ -27,11 +23,7 @@ nameInput.addEventListener('input', ()=>{
     }
 });
 
-//numero de la tarjeta
 
-let numberCard = document.querySelector('.card__front--number');
-let numberInput = document.querySelector('#cardnumber');
-let numberError = document.querySelector('.errorNumber');
 
 //regex y manejador de eventos
 
@@ -54,11 +46,7 @@ numberInput.addEventListener('input',(event)=>{
     (numberInput.value === '')? numberCard.textContent = '0000 0000 0000 0000': numberCard.textContent = numberInput.value;
 });
 
-//mes tarjeta
 
-let cardmonth = document.querySelector('.card__month');
-let inputmonth = document.querySelector('#cardmonth');
-let montherror = document.querySelector('.errorMonth');
 
 //regex y manejador de eventos
 
@@ -74,11 +62,7 @@ inputmonth.addEventListener('input', ()=>{
     }
 });
 
-//año tarjeta
 
-let cardyear = document.querySelector('.card__year');
-let inputCard = document.querySelector('#cardyear');
-let errorYear = document.querySelector('.errorYear');
 
 inputCard.addEventListener('input', ()=>{
     (inputCard.value === '')? cardyear.textContent = '00' : cardyear.textContent = inputCard.value;
@@ -93,11 +77,7 @@ inputCard.addEventListener('input', ()=>{
 });
 
 
-//cvc tarjeta
 
-let cvc_Card = document.querySelector('.card__back--cvc');
-let input_Cvc = document.querySelector('#cardcvc');
-let error_Cvc = document.querySelector('.errorCvc');
 
 input_Cvc.addEventListener('input',()=>{
     (input_Cvc.value === '')? cvc_Card.textContent = '000' : cvc_Card.textContent = input_Cvc.value;
@@ -114,21 +94,12 @@ input_Cvc.addEventListener('input',()=>{
 
 //validacion de envio de datos
 
-//seciciones finales
 
-let confirm = document.querySelector('.form__submit');
-let form = document.querySelector('.form');
-let gracias = document.querySelector('.completed');
-let correctname = true;
-let correctnumber = true;
-let correctmes = true;
-let correctano = true;
-let correctcvc = true;
 
 confirm.addEventListener('click', (event)=>{
     event.preventDefault();
     //validando nombre
-    if(nameInput.value === ''){
+    if(nameInput.value=== '' ){
         nameError.textContent = `Can't be blank`;
         nameInput.classList.add('cardholder');
         nameError.classList.remove('error');
@@ -141,8 +112,7 @@ confirm.addEventListener('click', (event)=>{
         numberInput.classList.add('cardholder');
         numberError.classList.remove('error');
         correctnumber = false;
-    }
-    if(numberInput.value.length < 19){
+    }else if(numberInput.value.length < 19){
         numberError.textContent = 'Wrong number';
         numberInput.classList.add('cardholder');
         numberError.classList.remove('error');
---
+++ nodos.js
@@ -0,0 +1,49 @@
+//nombre de la tarjeta 
+
+let nameCard = document.querySelector('.card__details--name');
+let nameInput = document.querySelector('#cardholder');
+let nameError = document.querySelector('.errorName');
+
+//numero de la tarjeta
+
+let numberCard = document.querySelector('.card__front--number');
+let numberInput = document.querySelector('#cardnumber');
+let numberError = document.querySelector('.errorNumber');
+
+//mes tarjeta
+
+let cardmonth = document.querySelector('.card__month');
+let inputmonth = document.querySelector('#cardmonth');
+let montherror = document.querySelector('.errorMonth');
+
+//año tarjeta
+
+let cardyear = document.querySelector('.card__year');
+let inputCard = document.querySelector('#cardyear');
+let errorYear = document.querySelector('.errorYear');
+
+//cvc tarjeta
+
+let cvc_Card = document.querySelector('.card__back--cvc');
+let input_Cvc = document.querySelector('#cardcvc');
+let error_Cvc = document.querySelector('.errorCvc');
+
+//seciciones finales
+
+let confirm = document.querySelector('.form__submit');
+let form = document.querySelector('.form');
+let gracias = document.querySelector('.completed');
+let correctname = true;
+let correctnumber = true;
+let correctmes = true;
+let correctano = true;
+let correctcvc = true;
+
+
+
+
+
+
+
+
+
